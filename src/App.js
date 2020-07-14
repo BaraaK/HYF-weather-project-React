32939388283bbcf73024ca4d88f72046dea80136
Baraa Kolly <baraa.ali.kolly@gmail.com>
2020-07-14 16:05:08 +0200

    week2 updating

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './App.css';
 import data from './city-weather.json'
-import CityCard from './cityCard';
+import CityCard from './component/cityCard';
+import Search from './component/search'
 
 function App() {
-  const cities = data.map(city => {
-  return {name : `${city.name}`,
-          country : `${city.sys.country}`,
-          weather: `${city.weather[0].main}`,
-          description: `${city.weather[0].description}`, 
-          max_temp : `${city.main.temp_max}`,
-          min_temp : `${city.main.temp_min}`,
-          lon : `${city.coord.lon}`,
-          lat : `${city.coord.lat}`
-        }    
-  })
-    console.log(cities)
-  
-  
+ 
   return (
     <div className="main">
-      <CityCard cities = {cities}/>
+      <h1>Weather</h1>
+      <div>
+        <Search/>
+      </div>
     </div>
+   
+
   );
 
 }
